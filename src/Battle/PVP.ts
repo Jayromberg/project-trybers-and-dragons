1f6ed5b60a1dc82062f263a2642cda10a381d763
Jayromberg <jayro.dev@gmail.com>
2022-10-19 18:45:17 -0300

    feat: Implementada a classe PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,32 @@
+import Character from '../Character';
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  private _player1: Fighter;
+  private _player2: Fighter;
+
+  constructor(
+    player1: Fighter = new Character('Tryber'),
+    player2: Fighter = new Character('Lalaland'),
+  ) {
+    super(player1);
+    this._player1 = player1;
+    this._player2 = player2;
+  }
+
+  fight(): number {
+    let lifePlayer1 = this._player1.lifePoints;
+    let lifePlayer2 = this._player2.lifePoints;
+
+    while (lifePlayer1 > 0 && lifePlayer2 > 0) {
+      this._player2.attack(this._player1);
+      this._player1.attack(this._player2);
+      
+      lifePlayer1 = this._player1.lifePoints;
+      lifePlayer2 = this._player2.lifePoints;
+    }
+
+    return super.fight();
+  }
+}
